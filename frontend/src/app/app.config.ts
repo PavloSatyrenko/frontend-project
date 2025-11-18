f382c2c6b05103902dfe87385723ac36427b037a
PavloSatyrenko <zevsp09@gmail.com>
2025-11-18 21:49:04 +0200

    Refactor app configuration and initialization logic in App component

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,20 +1,13 @@
 import {
-    provideAppInitializer,
     ApplicationConfig,
     provideBrowserGlobalErrorListeners,
     provideZonelessChangeDetection,
-    inject,
 } from "@angular/core";
 import { provideRouter } from "@angular/router";
 
 import { routes } from "./app.routes";
 import { provideHttpClient, withInterceptors } from "@angular/common/http";
 import { Interceptor } from "@core/interceptor";
-import { AuthService } from "@shared/services/auth.service";
-import { ConfigService } from "@shared/services/config.service";
-import { CartService } from "@shared/services/cart.service";
-import { UserRoleEnum } from "@shared/types/UserRoleEnum";
-import { ProductsService } from "@shared/services/products.service";
 
 export const appConfig: ApplicationConfig = {
     providers: [
@@ -22,20 +15,5 @@ export const appConfig: ApplicationConfig = {
         provideRouter(routes),
         provideHttpClient(withInterceptors([Interceptor])),
         provideZonelessChangeDetection(),
-        provideAppInitializer(async () => {
-            const configService: ConfigService = inject(ConfigService);
-            const authService: AuthService = inject(AuthService);
-            const cartService: CartService = inject(CartService);
-            const productsService: ProductsService = inject(ProductsService);
-
-            await configService.init();
-            await authService.init();
-
-            const user = authService.user();
-
-            cartService.init(!!user, user ? user.role : UserRoleEnum.Retail);
-
-            productsService.init(user ? user.role : UserRoleEnum.Retail);
-        }),
     ],
 };
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,30 @@
-import { Component } from "@angular/core";
+import { Component, inject, OnInit } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
+import { AuthService } from "@shared/services/auth.service";
+import { CartService } from "@shared/services/cart.service";
+import { ConfigService } from "@shared/services/config.service";
+import { ProductsService } from "@shared/services/products.service";
+import { UserRoleEnum } from "@shared/types/UserRoleEnum";
 
 @Component({
     selector: "app-root",
     imports: [RouterOutlet],
     template: "<router-outlet></router-outlet>",
 })
-export class App {}
+export class App implements OnInit {
+    private configService: ConfigService = inject(ConfigService);
+    private authService: AuthService = inject(AuthService);
+    private cartService: CartService = inject(CartService);
+    private productsService: ProductsService = inject(ProductsService);
+
+    public async ngOnInit(): Promise<void> {
+        await this.configService.init();
+        await this.authService.init();
+
+        const user = this.authService.user();
+
+        this.cartService.init(!!user, user ? user.role : UserRoleEnum.Retail);
+
+        this.productsService.init(user ? user.role : UserRoleEnum.Retail);
+    }
+}
